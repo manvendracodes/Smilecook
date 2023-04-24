3e6d3c4b75f97b860567bff943330621d0937bfd
Manvendraaaa <manubhadauria.1994@gmail.com>
2023-04-24 17:49:50 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:Manu%402000@localhost/smilecook'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
